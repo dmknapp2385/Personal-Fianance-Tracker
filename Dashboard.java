bd03bd933f34792946cab6b2b21bbdd63989f7b3
dmknapp2385 <dmknapp2385@gmail.com>
2024-11-14 11:15:36 -0700

    started working on dashboard panel

---
+++ Dashboard.java
@@ -0,0 +1,14 @@
+
+import javax.swing.JPanel;
+
+class Dashboard extends JPanel implements Observer{
+
+    public Dashboard(){
+        
+    }
+
+    @Override
+    public void budgetChange() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+}
