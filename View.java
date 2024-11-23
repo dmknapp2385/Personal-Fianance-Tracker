5d1d82bd3fb2aabb067549d008acddb5ef103168
dmknapp2385 <dmknapp2385@gmail.com>
2024-11-23 13:05:35 -0700

    modified fiance to finance

--- View.java
+++ View.java
@@ -79,7 +79,7 @@ public class View extends JFrame implements Observer {
         this.add(budgetView, "Budget");
 
         //create Finance View
-        FianceView financeView = new FianceView();
+        FinanceView financeView = new FinanceView();
         this.add(financeView, "Finance");
 
         //adding a window listener for closing the app
