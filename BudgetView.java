6ba8ce05689c892ec1e83a3ca390dde574e09a12
Chitrangada Juneja <chitrangada@Chitrangadas-MacBook-Air.local>
2024-11-26 17:45:31 -0700

    edited budget view

--- BudgetView.java
+++ BudgetView.java
@@ -1,13 +1,26 @@
 
+import java.awt.BorderLayout;
+
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class BudgetView extends JPanel implements Observer{
+	
+	
     public BudgetView(){
+    	
         View.controller.addObserver(this);
+        setLayout(new BorderLayout());
+        
 
         JLabel test = new JLabel("This is the budget page");
         this.add(test);
+        
+    }
+    
+    
+    public void setUpTable() {
+    	
     }
 
     @Override
@@ -18,6 +31,7 @@ public class BudgetView extends JPanel implements Observer{
 
     @Override
     public void loginChange() {
+    	
         
     }
 }
