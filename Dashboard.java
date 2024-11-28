350bc0b318d8838ca5c4f5fb099854f3c4b4fbdd
Chitrangada Juneja <chitrangada@Chitrangadas-MacBook-Air.local>
2024-11-28 15:03:54 -0700

    edited dashboard

--- Dashboard.java
+++ Dashboard.java
@@ -4,15 +4,20 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.SwingConstants;
+import javax.swing.SwingUtilities;
 
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
 import java.awt.Font;
+import java.time.LocalDate;
 import java.util.ArrayList;
 
 
 
 public class Dashboard extends JPanel implements Observer{
+	private JScrollPane expenseScrollPane;
+	private JPanel expensePanel;
+	private JLabel welcomeLabel;
 
     public Dashboard(){
         View.controller.addObserver(this);
@@ -26,7 +31,7 @@ public class Dashboard extends JPanel implements Observer{
     	
     	JPanel welcomePanel= new JPanel();
     	welcomePanel.setLayout(new FlowLayout(FlowLayout.CENTER)); //center alignment
-    	JLabel welcomeLabel = new JLabel();
+    	welcomeLabel = new JLabel();
     	String name=View.controller.getUserDetails();
     	if(name!=null) {
     		welcomeLabel.setText("Welcome, "+name+" to your Dashboard!");
@@ -39,47 +44,60 @@ public class Dashboard extends JPanel implements Observer{
     	welcomeLabel.setFont(new Font ("Arial", Font.BOLD, 16));
     	welcomePanel.add(welcomeLabel);
     	
-    	//displaying expenses
-    	
-    	JPanel expensesPanel= new JPanel();
-    	expensesPanel.setLayout(new BorderLayout());
+    	expensePanel= new JPanel();
+    	expensePanel.setLayout(new BorderLayout());
     	JLabel expensesLabel= new JLabel("Your Last 10 expenses: ");
     	expensesLabel.setFont(new Font("Arial", Font.PLAIN, 14));
-    	expensesPanel.add(expensesLabel, BorderLayout.NORTH);
+    	expensePanel.add(expensesLabel, BorderLayout.NORTH);
     	
-    	//Tabular display
-    	String [] columns= {"Date","Description","Category", "Amount"};
-    	ArrayList <Expense> displayExpenses= View.controller.sortedTenExpenses();
-    	if (displayExpenses==null || displayExpenses.isEmpty()) {
-    		JLabel noExpensesLabel= new JLabel("No expenses yet to display");
-    		noExpensesLabel.setHorizontalAlignment(SwingConstants.CENTER);
-    		noExpensesLabel.setFont(new Font("Arial", Font.ITALIC, 14));
-    		expensesPanel.add(noExpensesLabel,BorderLayout.CENTER);
-    		
-    		
-    	}
-    	else {
-    		Object[][] data= new Object [displayExpenses.size()][4];
-        	
-        	for ( int i=0; i<displayExpenses.size();i++) {
-        		Expense  e= displayExpenses.get(i);
-        		
-        		data[i][0]= e.getDate();
-        		data[i][1]=e.getDescription();
-        		data[i][2]=e.getCategory();
-        		data[i][3]=e.getAmount();
-        	}
-        	JTable expensesTable= new JTable(data, columns);
-        	
-        	JScrollPane scrollPane= new JScrollPane(expensesTable);
-        	expensesPanel.add(scrollPane, BorderLayout.CENTER);
-        	
-    	}
+    	
     	add(welcomePanel, BorderLayout.NORTH);
-    	add(expensesPanel, BorderLayout.CENTER);
+    	add(new JScrollPane(expensePanel), BorderLayout.CENTER);
+    	
+    	showTenExpenses(View.controller.sortedTenExpenses());
+    	
+    	
     	
     	
     }
+    private void showTenExpenses(ArrayList <Expense> sortedExpenses) {
+    	SwingUtilities.invokeLater(()->{
+    		expensePanel.removeAll();
+    		if (sortedExpenses==null || sortedExpenses.isEmpty()) {
+    			JLabel noExpensesLabel= new JLabel("No expenses yet to display");
+        		noExpensesLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        		noExpensesLabel.setFont(new Font("Arial", Font.ITALIC, 14));
+        		expensePanel.add(noExpensesLabel,BorderLayout.CENTER);
+        	
+        	}
+    		else {
+    			String [] columns= {"Date","Description","Category", "Amount"};
+    			Object[][] data= new Object [sortedExpenses.size()][4];
+            	
+            	for ( int i=0; i<sortedExpenses.size();i++) {
+            		Expense  e= sortedExpenses.get(i);
+            		
+            		data[i][0]= e.getDate();
+            		data[i][1]=e.getDescription();
+            		data[i][2]=e.getCategory();
+            		data[i][3]=e.getAmount();
+            	}
+            	JTable expensesTable= new JTable(data, columns);
+            	
+            	JScrollPane scrollPane= new JScrollPane(expensesTable);
+            	expensePanel.add(scrollPane, BorderLayout.CENTER);
+            	
+        	
+    			
+    		}
+    		expensePanel.revalidate();
+    		expensePanel.repaint();
+    		
+    		
+    		
+    	});
+    }
+    
 
     @Override
     public void budgetChange() {
@@ -87,19 +105,15 @@ public class Dashboard extends JPanel implements Observer{
         //update expense labels
         // need method in the user to get last 10 expesense
     	
-    	this.setup();
+    	showTenExpenses(View.controller.sortedTenExpenses());
     	
         
     }
     
     @Override
     public void loginChange() {
-       this.setup();
+    	showTenExpenses(View.controller.sortedTenExpenses());
     }
     
-    @Override
-    public void expenseChange() {
-    	this.setup();
-    	
-    }
+    
 }
